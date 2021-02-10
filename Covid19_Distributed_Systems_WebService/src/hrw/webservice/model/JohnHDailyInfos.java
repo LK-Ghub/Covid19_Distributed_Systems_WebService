1ccdb2fc9ef87e0770ce88b7f38c11858ecb4dc6
LK-Ghub <lkarbach@gmx.de>
2021-02-10 16:38:47 +0100

    Daily Info

---
+++ Covid19_Distributed_Systems_WebService/src/hrw/webservice/model/JohnHDailyInfos.java
@@ -0,0 +1,11 @@
+
+public class JohnHDailyInfos {
+	private String date;
+	private int confirmed;
+	private int deaths;
+	private int recovered;
+	
+	public void getDate(String date) {
+		this.date = date;
+	}
+}
